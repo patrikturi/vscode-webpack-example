a2f7da0e48717a4f8d66a06c6313bfac672f7f0b
Patrik Turi <patrik.turi.0xff@gmail.com>
2019-04-29 10:11:32 +0200

    Initial version

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+	"globals": {
+		"Phaser": true
+	},
+	"settings": {
+		"import/core-modules": ["phaser"]
+	}
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+Example project for Visual Studio Code with webpack and chrome.
+
+Don't forget to copy all non-javascript files from app/ to dist/.
+
+Usage:
+* npm build
+* npm dev
+* Start debugging in vscode
